f46df20b1a64e2792c4c0a3e8d6f59c5075e71b2
Rodolfo Alvarenga <rodolfo.l.alvarenga@gmail.com>
2020-12-02 18:44:55 -0300

    11. Movie Details

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import GlobalStyle from "./components/GlobalStyle";
 // Import pages
 import AboutUs from "./pages/AboutUs";
 import ContactUs from "./pages/ContactUs";
+import MovieDetail from "./pages/MovieDetail.js";
 import OurWork from "./pages/OurWork";
+// Components
 import Nav from "./components/Nav.js";
 // Router
 import { Switch, Route } from "react-router-dom";
@@ -17,8 +19,11 @@ function App() {
         <Route path="/" exact>
           <AboutUs />
         </Route>
-        <Route path="/work">
+        <Route path="/work" exact>
           <OurWork />
+        </Route>
+        <Route path="/work/:id">
+          <MovieDetail />
         </Route>
         <Route path="/contact">
           <ContactUs />
