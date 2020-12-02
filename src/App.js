b6ca7afd2eb76a85e0a10d65c760eecb066160d3
Rodolfo Alvarenga <rodolfo.l.alvarenga@gmail.com>
2020-12-02 13:40:52 -0300

    5. Intro To Styled Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+// Global style
+import GlobalStyle from "./components/GlobalStyle";
 // Import pages
 import AboutUs from "./pages/AboutUs";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
