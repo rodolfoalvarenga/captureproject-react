0bd95601aaf5b1a64c6daf66d22e459cfac54b8f
Rodolfo Alvarenga <rodolfo.l.alvarenga@gmail.com>
2020-12-03 10:44:45 -0300

    12. Frame Motion and Page Transitions

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,10 +1,19 @@
 import React from "react";
+// Animation
+import { motion } from "framer-motion";
+import { pageAnimation } from "../animation.js";
 
 const ContactUs = () => {
   return (
-    <div>
+    <motion.div
+      variants={pageAnimation}
+      initial="hidden"
+      animate="show"
+      exit="exit"
+      style={{ background: "#fff" }}
+    >
       <h1>Our Contact</h1>
-    </div>
+    </motion.div>
   );
 };
 
